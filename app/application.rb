25ff77bac4d4af5d761c1bae9ccec9a0c57e2d36
Kristen Peck <kpeck71@gmail.com>
2017-12-04 01:41:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,11 +7,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      Item.price
-    else
-      resp.status = 400
-      resp.write "We don't have that item"
-    end
+      new_item = req.params["item"]
+      if items.include? new_item
+        new_item.price
+      else
+        resp.status = 400
+        resp.write "We don't have that item"
+      end
   end
 
 end
