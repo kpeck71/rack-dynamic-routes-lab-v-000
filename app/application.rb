f3d081eef63524e6f139fb37037cad7e5d92ad3a
Kristen Peck <kpeck71@gmail.com>
2017-12-04 01:39:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,14 @@
 class Application
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       Item.price
-      end
+    else
+      resp.status = 404
+    end
+  end
+  
+end
